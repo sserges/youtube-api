dfe7956467e6ac7ddec23cefb2ebca99b8edf2ac
sserges <agounkeserge@gmail.com>
2020-08-10 11:52:14 +0000

    youtube api tested successfully

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React from 'react';
 
+import { Grid } from '@material-ui/core';
+
+import { SearchBar, VideoDetail } from './components';
+
+import youtube from './api/youtube';
+
 class App extends React.Component {
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: 'AIzaSyDSldaV2VveqV7JfyUGKSGO_mABMRzB5Nc',
+        q: searchTerm,
+      },
+    });
+
+    console.log(response.data);
+  };
+
   render() {
-    return <h1>YouTube Clone App</h1>;
+    return (
+      <Grid justify='center' container spacing={10}>
+        <Grid item xs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetail />
+            </Grid>
+            <Grid item xs={4}>
+              {/* VIDEO LIST */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -2,9 +2,4 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
-  params: {
-    part: 'snippet',
-    maxResults: 5,
-    key: 'AIzaSyDSldaV2VveqV7JfyUGKSGO_mABMRzB5Nc',
-  },
 });
